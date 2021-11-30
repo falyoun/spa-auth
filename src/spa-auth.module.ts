38168c37df177c9c2947ebe59e1e96f93495ed1c
Abdulrahman Falyoun <falyoun.abdulrahman@gmail.com>
2021-11-30 10:07:14 +0200

    [ADD]: typescript configuration & basic files

---
+++ src/spa-auth.module.ts
@@ -0,0 +1,8 @@
+import { Module } from '@nestjs/common';
+import { SpaAuthService } from './services';
+@Module({
+  imports: [],
+  controllers: [],
+  providers: [SpaAuthService],
+})
+export class SpaAuthModule {}
