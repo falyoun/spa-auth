f1ab80f3e5f5d1b81a17fd24d32ec870659fcca8
Abdulrahman Falyoun <falyoun.abdulrahman@gmail.com>
2021-12-01 14:18:58 +0200

    [ADD]: Registering spa-auth.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SpaAuthModule } from '@app/spa-auth.module';
+import { AuthController } from '@app/auth.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [
+    SpaAuthModule.register({
+      useAccessToken: {
+        jwtAccessSecretKey: 'some_secret_key',
+        jwtAccessActivationPeriod: '5 minutes',
+      },
+      useRefreshToken: {
+        jwtRefreshSecretKey: 'jwt_refresh',
+        jwtRefreshActivationPeriod: '15 days',
+      },
+    }),
+  ],
+  controllers: [AuthController],
   providers: [AppService],
 })
 export class AppModule {}
