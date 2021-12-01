bc2bc77b26533397ea7621c8126a5565ff57e147
Abdulrahman Falyoun <falyoun.abdulrahman@gmail.com>
2021-12-01 14:17:21 +0200

    [ADD]: dynamic module for spa-auth.module.ts

--- src/spa-auth.module.ts
+++ src/spa-auth.module.ts
@@ -1,8 +1,94 @@
-import { Module } from '@nestjs/common';
+import { DynamicModule, Module, Provider } from '@nestjs/common';
 import { SpaAuthService } from './services';
-@Module({
-  imports: [],
-  controllers: [],
-  providers: [SpaAuthService],
-})
-export class SpaAuthModule {}
+import { JwtModule } from '@nestjs/jwt';
+import {
+  SpaAuthConstants,
+  SpaAuthModuleAsyncOptions,
+  SpaAuthModuleOptionsFactory,
+  SpaAuthOptions,
+} from '@app/shared';
+import { JwtStrategy } from '@app/strategies';
+@Module({})
+export class SpaAuthModule {
+  static register(options: SpaAuthOptions): DynamicModule {
+    return {
+      module: SpaAuthModule,
+      imports: [
+        JwtModule.register({
+          secret: options.useAccessToken.jwtAccessSecretKey,
+        }),
+      ],
+      providers: [
+        {
+          provide: SpaAuthConstants.SPA_AUTH_MODULE_OPTIONS,
+          useValue: options,
+        },
+        {
+          provide: SpaAuthConstants.SPA_AUTH_SERVICE_TOKEN,
+          useClass: SpaAuthService,
+        },
+        JwtStrategy,
+      ],
+      exports: [
+        {
+          provide: SpaAuthConstants.SPA_AUTH_SERVICE_TOKEN,
+          useClass: SpaAuthService,
+        },
+      ],
+    };
+  }
+
+  static registerAsync(options: SpaAuthModuleAsyncOptions): DynamicModule {
+    return {
+      module: SpaAuthModule,
+      imports: options.imports,
+      providers: [
+        ...this.createAsyncProviders(options),
+        {
+          provide: SpaAuthConstants.SPA_AUTH_SERVICE_TOKEN,
+          useClass: SpaAuthService,
+        },
+        ...(options.extraProviders || []),
+      ],
+      exports: [
+        {
+          provide: SpaAuthConstants.SPA_AUTH_SERVICE_TOKEN,
+          useClass: SpaAuthService,
+        },
+      ],
+    };
+  }
+
+  private static createAsyncProviders(
+    options: SpaAuthModuleAsyncOptions,
+  ): Provider[] {
+    if (options.useExisting || options.useFactory) {
+      return [this.createAsyncOptionsProvider(options)];
+    }
+    return [
+      this.createAsyncOptionsProvider(options),
+      {
+        provide: options.useClass,
+        useClass: options.useClass,
+      },
+    ];
+  }
+
+  private static createAsyncOptionsProvider(
+    options: SpaAuthModuleAsyncOptions,
+  ): Provider {
+    if (options.useFactory) {
+      return {
+        provide: SpaAuthConstants.SPA_AUTH_MODULE_OPTIONS,
+        useFactory: options.useFactory,
+        inject: options.inject || [],
+      };
+    }
+    return {
+      provide: SpaAuthConstants.SPA_AUTH_MODULE_OPTIONS,
+      useFactory: async (optionsFactory: SpaAuthModuleOptionsFactory) =>
+        optionsFactory.createSpaAuthModuleOptions(),
+      inject: [options.useExisting || options.useClass],
+    };
+  }
+}
